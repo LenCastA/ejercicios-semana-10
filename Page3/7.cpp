a86c1696a2c5126bfb025cb77cb0ef3bce8ea52c
Lenin Castro <lenin.castro.a@uni.pe>
2024-06-05 00:11:24 -0500

    change page3_7

--- Page3/7.cpp
+++ Page3/7.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int n = 0;
+    int contador = 0;
+    long long num = 0;
 
-    while (n <= 0){
-        cout << "Ingrese un numero: "; cin >> n;
-    }
+    cout << "Ingrese un numero: "; cin >> num;
 
-    cout << "Divisores de " << n << ": ";
-    
-    for (int i = 1; i <= n; i++){
-        if (n % i == 0){
-            cout << i << " ";
+    for (int i = 1; i <= sqrt(num); i++){
+        if (num % i == 0 &&  i != num/i){
+            contador += 2;
+            cout << i << " " << num/i << endl;
+        } else if (num % i == 0 && i == num/i){
+            contador++;
+            cout << i << " " << num/i << endl;
         }
     }
+    cout << "El numero " << num << " tiene " << contador << " divisores" << endl;
     return 0;
 }
